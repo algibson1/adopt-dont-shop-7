b33a26b4e6ccf0bc42d7365470e9ca22e25b956e
Artemy Gibson <artemygibson@gmail.com>
2023-07-17 15:35:02 -0700

    Test that approving/rejecting a pet doesn't affect other apps

--- spec/features/admin/application/show_spec.rb
+++ spec/features/admin/application/show_spec.rb
@@ -18,6 +18,7 @@ RSpec.describe 'Admin Application Show Page' do
     visit "/admin/applications/#{phylis.id}"
 
     expect(page).to have_button("Reject")
+    expect(page).to have_button("Accept")
 
     within "#pet-#{sparky.id}" do
       click_button "Reject"
@@ -37,6 +38,7 @@ RSpec.describe 'Admin Application Show Page' do
     visit "/admin/applications/#{phylis.id}"
 
     expect(page).to have_button("Accept")
+    expect(page).to have_button("Reject")
 
     within "#pet-#{sparky.id}" do
       click_button "Accept"
@@ -45,5 +47,89 @@ RSpec.describe 'Admin Application Show Page' do
       expect(page).not_to have_button("Accept")
     end
 
+  end
+
+  # 14. Approved/Rejected Pets on one Application do not affect other Applications
+
+  #   As a visitor
+  #   When there are two applications in the system for the same pet
+  #   When I visit the admin application show page for one of the applications
+  #   And I approve or reject the pet for that application
+  #   When I visit the other application's admin show page
+  #   Then I do not see that the pet has been accepted or rejected for that application
+  #   And instead I see buttons to approve or reject the pet for this specific application
+
+  it "does not affect other petapplications when one is accepted" do
+    happy_tails = Shelter.create!(name: "Happy Tails", foster_program: true, city: "San Francisco", rank: 3)
+    sparky = happy_tails.pets.create!(name: "Sparky", adoptable: true, age: 2, breed: "Beagle")
+    phylis = Application.create!(name: "Phylis", street_address: "1234 main circle", city: "Littleton", state: "CO", zipcode: "80241", reason_for_adoption: "I have a huge yard", status: "Pending")
+    PetApplication.create!(application: phylis, pet: sparky, status: "Pending")
+    johnny = Application.create!(name: 'Johnny', street_address: '1234 main st.', city: 'Westminster', state: 'CO', zipcode: '80241',  reason_for_adoption: "I love animals", status: "Pending" )
+    PetApplication.create!(application: johnny, pet: sparky, status: "Pending")
+
+    visit "/admin/applications/#{phylis.id}"
+
+    within "#pet-#{sparky.id}" do
+      expect(page).to have_content("Accept")
+      expect(page).to have_content("Reject")
+      expect(page).to have_content("Pending")
+    end
+
+    visit "/admin/applications/#{johnny.id}"
+
+    within "#pet-#{sparky.id}" do
+      expect(page).to have_button("Accept")
+      expect(page).to have_button("Reject")
+      expect(page).to have_content("Pending")
+      click_button("Accept")
+      expect(page).to_not have_button("Accept")
+      expect(page).to_not have_button("Reject")
+      expect(page).to have_content("Accepted")
+    end
+
+    visit "/admin/applications/#{phylis.id}"
+
+    within "#pet-#{sparky.id}" do
+      expect(page).to have_content("Accept")
+      expect(page).to have_content("Reject")
+      expect(page).to have_content("Pending")
+    end
+  end
+
+  it "does not affect other petapplications when one is rejected" do
+    happy_tails = Shelter.create!(name: "Happy Tails", foster_program: true, city: "San Francisco", rank: 3)
+    sparky = happy_tails.pets.create!(name: "Sparky", adoptable: true, age: 2, breed: "Beagle")
+    phylis = Application.create!(name: "Phylis", street_address: "1234 main circle", city: "Littleton", state: "CO", zipcode: "80241", reason_for_adoption: "I have a huge yard", status: "Pending")
+    PetApplication.create!(application: phylis, pet: sparky, status: "Pending")
+    johnny = Application.create!(name: 'Johnny', street_address: '1234 main st.', city: 'Westminster', state: 'CO', zipcode: '80241',  reason_for_adoption: "I love animals", status: "Pending" )
+    PetApplication.create!(application: johnny, pet: sparky, status: "Pending")
+    
+    visit "/admin/applications/#{phylis.id}"
+
+    within "#pet-#{sparky.id}" do
+      expect(page).to have_content("Accept")
+      expect(page).to have_content("Reject")
+      expect(page).to have_content("Pending")
+    end
+
+    visit "/admin/applications/#{johnny.id}"
+
+    within "#pet-#{sparky.id}" do
+      expect(page).to have_button("Accept")
+      expect(page).to have_button("Reject")
+      expect(page).to have_content("Pending")
+      click_button("Reject")
+      expect(page).to_not have_button("Accept")
+      expect(page).to_not have_button("Reject")
+      expect(page).to have_content("Rejected")
+    end
+
+    visit "/admin/applications/#{phylis.id}"
+
+    within "#pet-#{sparky.id}" do
+      expect(page).to have_content("Accept")
+      expect(page).to have_content("Reject")
+      expect(page).to have_content("Pending")
+    end
   end
 end
