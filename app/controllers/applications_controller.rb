dc4c28774da7174dd75e11d6a157cecc64fc77a8
Artemy Gibson <artemygibson@gmail.com>
2024-07-19 20:15:54 -0700

    Change update method to utilize application_params

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,12 +1,10 @@
 class ApplicationsController < ApplicationController
-
+  before_action :find_app, only: [:show, :update]
   def show
-    @application = Application.find(params[:id])
     @results = Pet.search(params[:search]) if params[:search]
   end
 
   def new
-
   end
 
   def create
@@ -20,21 +18,20 @@ class ApplicationsController < ApplicationController
   end
 
   def update
-    application = Application.find(params[:id])
-    if params[:reason_for_adoption].empty?
-      redirect_to "/applications/#{application.id}"
-      flash[:alert] = "Please input why you would be a good home"
+    if @application.update(application_params)
+      redirect_to "/applications/#{@application.id}"
     else
-      application.update(
-        status: "Pending",
-        reason_for_adoption: params[:reason_for_adoption]
-      )
-      redirect_to "/applications/#{application.id}"
+      redirect_to "/applications/#{@application.id}"
+      flash[:alert] = "Error: #{error_message(@application.errors)}"
     end
   end
 
   private
   def application_params
-    params.permit(:name, :street_address, :city, :state, :zipcode)
+    params.permit(:name, :street_address, :city, :state, :zipcode, :status, :reason_for_adoption)
+  end
+
+  def find_app
+    @application = Application.find(params[:id])
   end
 end
