a75b81a967fc582403faa6849dae150a707ab347
Artemy Gibson <artemygibson@gmail.com>
2024-07-19 18:32:18 -0700

    Remove pet_status test, add has_pets? test

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -13,10 +13,11 @@ RSpec.describe Application do
     it {should validate_presence_of :state}
     it {should validate_presence_of :zipcode}
     it {should validate_presence_of :reason_for_adoption}
+    it {should validate_presence_of :status}
   end
 
-  describe "pet_status" do
-    it "finds the approved/rejected status of a pet on a specific application" do
+  describe "has_pets?" do
+    it "can report if an application has pets" do
       happy_tails = Shelter.create!(name: "Happy Tails", foster_program: true, city: "San Francisco", rank: 3)
       sparky = happy_tails.pets.create!(name: "Sparky", adoptable: true, age: 2, breed: "Beagle")
       rufus = happy_tails.pets.create!(name: "Rufus", adoptable: false, age: 7, breed: "Bloodhound")
@@ -24,17 +25,17 @@ RSpec.describe Application do
       phylis = Application.create!(name: "Phylis", street_address: "1234 main circle", city: "Littleton", state: "CO", zipcode: "80241", reason_for_adoption: "I have a huge yard", status: "Pending")
       joe = Application.create!(name: "Joe", street_address: "1234 main circle", city: "Littleton", state: "CO", zipcode: "80241", reason_for_adoption: "I have a huge yard", status: "Pending")
 
-      PetApplication.create!(pet_id: sparky.id, application_id: phylis.id, status: "Approved")
-      PetApplication.create!(pet_id: rufus.id, application_id: phylis.id, status: "Pending")
-      PetApplication.create!(pet_id: sparky.id, application_id: joe.id, status: "Pending")
-      PetApplication.create!(pet_id: rufus.id, application_id: joe.id, status: "Rejected")
+      expect(phylis.has_pets?).to eq(false)
+      expect(joe.has_pets?).to eq(false)
 
-      expect(phylis.pet_status(sparky)).to eq("Approved")
-      expect(joe.pet_status(sparky)).to eq("Pending")
-      expect(phylis.pet_status(rufus)).to eq("Pending")
-      expect(joe.pet_status(rufus)).to eq("Rejected")
-      expect(phylis.status).to eq("Pending")
-      expect(joe.status).to eq("Pending")
+      PetApplication.create!(pet_id: sparky.id, application_id: phylis.id, status: "Pending")
+      PetApplication.create!(pet_id: rufus.id, application_id: phylis.id, status: "Pending")
+
+      expect(phylis.has_pets?).to eq(true)
+      expect(joe.has_pets?).to eq(false)
+
+      PetApplication.create!(pet_id: sparky.id, application_id: joe.id, status: "Pending")
+      expect(joe.has_pets?).to eq(true)
     end
   end
 end
