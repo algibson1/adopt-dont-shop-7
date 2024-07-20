bb3f145709396c25e29893f32bfe329863d718e6
Artemy Gibson <artemygibson@gmail.com>
2024-07-19 20:17:00 -0700

    Remove .where AR method and use .find_by, clean up create method

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,17 +1,12 @@
 class PetApplicationsController < ApplicationController
-    def create
-        applicant = Application.find(params[:id])
-        petapp = PetApplication.create!(
-            pet_id: params[:pet_id],
-            application_id: params[:id],
-            status: "Pending"
-            )
-        redirect_to "/applications/#{applicant.id}"
-    end
+  def create
+    PetApplication.create(pet_id: params[:pet_id],application_id: params[:id])
+    redirect_to "/applications/#{params[:id]}"
+  end
 
-    def update
-        pet_application = PetApplication.where(application_id: params[:id]).where(pet_id: params[:pet_id]).first
-        pet_application.update!(status: params[:status])
-        redirect_to "/admin/applications/#{params[:id]}"
-    end
+  def update
+    pet_application = PetApplication.find_by(application_id: params[:app_id], pet_id: params[:pet_id])
+    pet_application.update(status: params[:status])
+    redirect_to "/admin/applications/#{params[:app_id]}"
+  end
 end
