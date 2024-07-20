902d23bc504dc597551a3f82f901d577535fcf52
Artemy Gibson <artemygibson@gmail.com>
2024-07-19 18:27:16 -0700

    Change syntax for validations, remove pet_status method and add has_pets? method

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,16 +1,16 @@
 class Application < ApplicationRecord
-    validates :name, presence: true
-    validates :street_address, presence: true
-    validates :city, presence: true
-    validates :state, presence: true
-    validates :zipcode, presence: true
-    validates :reason_for_adoption, presence: true
-    
-    has_many :pet_applications
-    has_many :pets, through: :pet_applications
+  validates_presence_of :name,
+                        :street_address,
+                        :city,
+                        :state,
+                        :zipcode,
+                        :reason_for_adoption,
+                        :status
+  
+  has_many :pet_applications
+  has_many :pets, through: :pet_applications
 
-    def pet_status(pet)
-        pet_application = PetApplication.where(application_id: self.id).where(pet_id: pet.id).first
-        pet_application.status
-    end
+  def has_pets?
+    !pets.empty?
+  end
 end
